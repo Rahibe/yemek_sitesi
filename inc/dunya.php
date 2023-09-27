b53cee78b772b5ac8132f573bc6041c153dd0f01
Rahibe <211603077@ogr.cbu.edu.tr>
2023-09-27 16:41:22 +0300

    Add files via upload

---
+++ inc/dunya.php
@@ -0,0 +1,98 @@
+<div class="bg_gray">
+    <div class="container margin_60_40">
+        <div class="main_title center">
+            <span><em></em></span>
+            <h2>Ülke Lezzetleri</h2>
+            <p>Dünya Mutfakları</p>
+        </div>
+        <!-- /main_title -->
+        <div class="owl-carousel owl-theme categories_carousel">
+            <div class="item_version_2">
+                <a href="grid-listing-filterscol.html">
+                    <figure>
+                        <span>98</span>
+                        <img src="img/home_cat_placeholder.jpg" data-src="img/home_cat_pizza.jpg" alt="" class="owl-lazy">
+                        <div class="info">
+                            <h3>Pizza</h3>
+                            <small>Avg price $40</small>
+                        </div>
+                    </figure>
+                </a>
+            </div>
+            <div class="item_version_2">
+                <a href="grid-listing-filterscol.html">
+                    <figure>
+                        <span>87</span>
+                        <img src="img/home_cat_placeholder.jpg" data-src="img/home_cat_sushi.jpg" alt="" class="owl-lazy">
+                        <div class="info">
+                            <h3>Japanese</h3>
+                            <small>Avg price $50</small>
+                        </div>
+                    </figure>
+                </a>
+            </div>
+            <div class="item_version_2">
+                <a href="grid-listing-filterscol.html">
+                    <figure>
+                        <span>55</span>
+                        <img src="img/home_cat_placeholder.jpg" data-src="img/home_cat_hamburgher.jpg" alt="" class="owl-lazy">
+                        <div class="info">
+                            <h3>Burghers</h3>
+                            <small>Avg price $55</small>
+                        </div>
+                    </figure>
+                </a>
+            </div>
+            <div class="item_version_2">
+                <a href="grid-listing-filterscol.html">
+                    <figure>
+                        <span>55</span>
+                        <img src="img/home_cat_placeholder.jpg" data-src="img/home_cat_vegetarian.jpg" alt="" class="owl-lazy">
+                        <div class="info">
+                            <h3>Vegetarian</h3>
+                            <small>Avg price $40</small>
+                        </div>
+                    </figure>
+                </a>
+            </div>
+            <div class="item_version_2">
+                <a href="grid-listing-filterscol.html">
+                    <figure>
+                        <span>65</span>
+                        <img src="img/home_cat_placeholder.jpg" data-src="img/home_cat_bakery.jpg" alt="" class="owl-lazy">
+                        <div class="info">
+                            <h3>Bakery</h3>
+                            <small>Avg price $60</small>
+                        </div>
+                    </figure>
+                </a>
+            </div>
+            <div class="item_version_2">
+                <a href="grid-listing-filterscol.html">
+                    <figure>
+                        <span>25</span>
+                        <img src="img/home_cat_placeholder.jpg" data-src="img/home_cat_chinesse.jpg" alt="" class="owl-lazy">
+                        <div class="info">
+                            <h3>Chinese</h3>
+                            <small>Avg price $40</small>
+                        </div>
+                    </figure>
+                </a>
+            </div>
+            <div class="item_version_2">
+                <a href="grid-listing-filterscol.html">
+                    <figure>
+                        <span>35</span>
+                        <img src="img/home_cat_placeholder.jpg" data-src="img/home_cat_mexican.jpg" alt="" class="owl-lazy">
+                        <div class="info">
+                            <h3>Mexican</h3>
+                            <small>Avg price $35</small>
+                        </div>
+                    </figure>
+                </a>
+            </div>
+        </div>
+        <!-- /carousel -->
+    </div>
+    <!-- /container -->
+</div>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php include ('inc/header.php'); ?>
+	<!-- /header -->
+	
+	<main>
+        <?php include ('inc/slider.php'); ?>
+		<!--/carousel-->
+        <?php include ('inc/dunya.php'); ?>
+		
+		<!-- /bg_gray -->
+		<!--- deneme -->
+
+        <?php include ('inc/onecıkan.php'); ?>
+		<!--- denme son -->
+
+
+
+
+
+
+        <?php include ('inc/videotarif.php'); ?>
+		<!-- /container -->
+
+		
+   		<!--/call_section-->
+		
+	</main>
+	<!-- /main -->
+
+<?php include ('inc/footer.php'); ?>
